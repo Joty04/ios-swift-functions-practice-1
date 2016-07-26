17d213093107e2af15d4deaf921f84fb3ffbc86e
dfenjves <fenjves.daniel@gmail.com>
2016-07-25 21:12:50 -0400

    major overhaul

--- FunctionsPractice1.playground/Contents.swift
+++ FunctionsPractice1.playground/Contents.swift
@@ -1,104 +1,27 @@
 import UIKit
 
 
-//Functions without return values
-//Max greets people differently depending on his mood. Make four methods: 
-//happyGreeting, angryGreeting, confusedGreeting, and tiredGreeting. 
-//Make each of them print a greeting matching their name. Do not give it a return value."
+//Functions Practice #1
 
+//Welcome back! This will be your first interactive practice on Functions! Remember that a function is just a reusable block of code that can be run by calling it by name.
 
-//define happyGreeting here
+// There are 4 pieces: the function name, the input (and input type_, the output (or return type), and the code to be run when the function gets called. Take a crack at building the functions below. We've done the first one for you...
 
+// #1) Build a function timesByOneThousand that takes an integer and multiplies it by 1000.
 
-
-
-
-
-//define angryGreeting here
-
-
-
-
-
-
-//define confusedGreeting here
-
-
-
-
-
-
-//define tiredGreeting here
-
-
-
-
-
-
-
-
-
-var mood: String = "" //Change the value of mood from happy, angry, confused, to tired.
-
-print("Hi Max, what's up?")
-
-//uncomment the function calls after you define them
-if mood == "happy" {
-    //happyGreeting()
-} else if mood == "angry" {
-    //angryGreeting()
-} else if mood == "confused" {
-    //confusedGreeting()
-} else if mood == "tired" {
-    //tiredGreeting()
+func timesByOneThousand(number:Int) -> Int {
+    let x:Int = number * 1000
+    return x
 }
 
+timesByOneThousand(11)
+timesByOneThousand(23)
 
-//------------------------------------------------------------------------------------------------------
-//Functions with return values
+// #2) Build a function howOldIWillBeIn2030 that takes in your age (as an integer) and tells you how old you will be in the year 2030.
 
-//make a function called myAge that returns the integer value of your age
+// #3) Build a function makeExcited that takes a string, makes it all uppercase and adds three exclamation points.
 
+// #4) Build a function called isEven that returns a boolean (true or false) depending on whether or not the number is divisible by 2.
 
-
-
-
-
-//make a function called fullName that returns a string containing your full name
-
-
-
-
-
-//make a function called homeTown that returns a string containing your hometown
-
-
-
-
-
-//uncomment this print statement once you finish the functions
-//print("Hi! My name is \(fullName()). I am \(myAge()) and I'm from \(homeTown()).")
-
-
-
-
-
-
-
-
-
-
-//BONUS: Write a function called inchesToCentimeters that takes in a number of inches and returns the equivalent 
-//amount of centimeters (You'll have to look up how to use arguments...)
-
-
-
-
-
-
-
-
-
-
-
+// #5) Build a function called gradeCalculator that takes in a number (0 to 100) and returns the letter grade that corresponds to it.
 
